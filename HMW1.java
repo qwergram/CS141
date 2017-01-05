f7be678781d08c005e615ebbf0ba2a44b48f4b32
Norton Pengra <npengra317@gmail.com>
2017-01-04 19:15:43 -0800

    Update HMW1.java

--- HMW1.java
+++ HMW1.java
@@ -9,13 +9,16 @@ package javaapplication1;
  *
  * @author norton
  */
+
+
 public class JavaApplication1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        polynomialTable(3, 4);
+        PopulationEstimate();
+        // polynomialTable(3, 4);
     }
     
     // Assignment 1.1
@@ -60,5 +63,83 @@ public class JavaApplication1 {
             }
     
         }
+    }
+    // Assignment 1.5
+    public static void weirdProblem() {
+        double result = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+        System.out.println(result);
+    }
+    
+    // Assignment 1.6
+    public static void weirdProblem2() {
+        int result = 0;
+        for (int toAdd=1; toAdd < 10; toAdd++) {
+            result += toAdd;
+        }
+        System.out.println(result);
+    }
+    
+    // Assignment 1.7
+    public static void approxPi() {
+        double pi = 1;
+        double denominatorStart = 3;
+        double denominatorMax = 11;
+        boolean positive = false;
+        for (double denominator = denominatorStart; denominator <= denominatorMax; denominator++) {
+            if (positive) {
+                pi += (1.0 / denominator);
+            } else {
+                pi -= (1.0 / denominator);
+            }
+            positive = !positive;
+        }
+        pi *= 4;
+        // emphasis on "approx"
+        System.out.println(pi);
+    }
+    
+    // Assignment 1.8
+    public static void CircleAreaAndPerim() {
+        double radius = 5.5;
+        double perimeter = Math.PI * 2.0 * radius;
+        double area = Math.PI * radius * radius;
+        System.out.println(perimeter);
+        System.out.println(area);
+    }
+    
+    // Assignment 1.9
+    public static void RectangleArea() {
+        double height = 7.9;
+        double width = 4.5;
+        double area = height * width;
+        double perimeter = height + height + width + width;
+        System.out.println(area);
+        System.out.println(perimeter);
+    }
+    
+    // Assignment 1.10
+    public static void AvgSpeed() {
+        double distanceRan = 14.0 * 1.6;
+        double mph = distanceRan / (45.5 / 60.0);
+        System.out.println(mph);
+        
+    }
+    
+    // Assignment 1.11
+    public static void PopulationEstimate() {
+        int birthsPerSecond = 7;
+        int deathsPerSecond = 13;
+        int immigrantsPerSecond = 45;
+        long currentPopulation = 312032486;
+        int yearLength = 365;
+        int simulationLengthYears = 5;
+        
+        long secondsInYear = yearLength * 24 * 60 * 60;
+        long futureBirths = (simulationLengthYears * secondsInYear) / birthsPerSecond;
+        long futureDeaths = (simulationLengthYears * secondsInYear) / deathsPerSecond;
+        long futureImmigrants = (simulationLengthYears * secondsInYear) / immigrantsPerSecond;
+        long futurePopulation = currentPopulation + futureBirths + futureImmigrants - futureDeaths;
+        System.out.println(futurePopulation);
+        
     }
 }
